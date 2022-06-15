9cfc7f3678c4057a7e90f8c0ac5c12f2e7b9052a
Shirakayo <46552362+Shirakayo@users.noreply.github.com>
2022-06-15 16:56:46 +0500

    Class Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,78 +1,63 @@
 import style from "./Users.module.css";
+import axios from "axios";
+import userPhoto from '../../assets/images/user.jpg'
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-      props.setUsers([
-          {
-              id: 1,
-              photoUrl:
-                  "https://i.pinimg.com/474x/97/3a/fe/973afe1eedc13510cab06492c151b581.jpg",
-              followed: false,
-              fullName: "Dmitriy",
-              status: "I am a boss!",
-              location: {city: "Moscow", country: "Russia"},
-          },
-          {
-              id: 2,
-              photoUrl:
-                  "https://i.pinimg.com/474x/97/3a/fe/973afe1eedc13510cab06492c151b581.jpg",
-              followed: true,
-              fullName: "Sergey",
-              status: "I am a noob!",
-              location: {city: "Minsk", country: "Belarus"},
-          },
-          {
-              id: 3,
-              photoUrl:
-                  "https://i.pinimg.com/474x/97/3a/fe/973afe1eedc13510cab06492c151b581.jpg",
-              followed: false,
-              fullName: "Sasha",
-              status: "I am a child!",
-              location: {city: "Kiev", country: "Ukraine"},
-          },
-      ]);
-  }
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                debugger
+                props.setUsers(response.data.items)
+            })
+        }
+    }
+
 
   return (
-    <div className={style.main}>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img src={u.photoUrl} className={style.image} alt={"avatar"} />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{u.fullName}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
-            </span>
-          </span>
+    <div>
+        <button className={style.btn} onClick={getUsers}>Get Users</button>
+        <div className={style.main}>
+              {props.users.map((u) => (
+                <div key={u.id}>
+                  <span>
+                    <div>
+                      <img src={u.photos.small != null ? u.photos.small : userPhoto} className={style.image} alt={"avatar"} />
+                    </div>
+                    <div>
+                      {u.followed ? (
+                        <button
+                            className={style.btn}
+                          onClick={() => {
+                            props.unfollow(u.id);
+                          }}
+                        >
+                          Unfollow
+                        </button>
+                      ) : (
+                        <button
+                            className={style.btn}
+                          onClick={() => {
+                            props.follow(u.id);
+                          }}
+                        >
+                          Follow
+                        </button>
+                      )}
+                    </div>
+                  </span>
+                  <span>
+                    <span>
+                      <div>{u.name}</div>
+                      <div>{u.status}</div>
+                    </span>
+                    <span>
+                      <div>{"u.location.country"}</div>
+                      <div>{"u.location.city"}</div>
+                    </span>
+                  </span>
+                </div>
+              ))}
         </div>
-      ))}
     </div>
   );
 };
