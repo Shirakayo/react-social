a3a7ddf9d2136667368fd328154dd9a41cdcbbbb
Shirakayo <46552362+Shirakayo@users.noreply.github.com>
2022-06-19 01:29:44 +0500

    add fetch loading and pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,83 +1,72 @@
+import React from 'react'
 import style from "./Users.module.css";
-import axios from "axios";
-import userPhoto from '../../assets/images/user.jpg'
-import React from 'react';
+import userPhoto from "../../assets/images/user.jpg";
 
-
-class Users extends React.Component {
-
-    componentDidMount() {
-        if (this.props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            })
-        }
+let Users = (props) => {
+    let pages = [];
+    for (let i = 1; i <= 10; i++) {
+        pages.push(i);
     }
-    render = () => {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-
-
-        let pages = []
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        return (
-            <div>
+    return (
+        <div>
+            <div className={style.main}>
+                {props.users.map((u) => (
+                    <div key={u.id}>
+              <span>
                 <div>
-                    {pages.map(p => {
-                        return (
-                        <span className={this.props.currentPage === p && style.active}>{p}</span>
-                        )
-                    })}
+                  <img src={userPhoto} className={style.image} alt={"avatar"}/>
                 </div>
-                <div className={style.main}>
-                    {this.props.users.map((u) => (
-                        <div key={u.id}>
-                  <span>
-                    <div>
-                      <img src={u.photos.small != null ? u.photos.small : userPhoto} className={style.image}
-                           alt={"avatar"}/>
-                    </div>
-                    <div>
-                      {u.followed ? (
-                          <button
-                              className={style.btn}
-                              onClick={() => {
-                                  this.props.unfollow(u.id);
-                              }}
-                          >
-                              Unfollow
-                          </button>
-                      ) : (
-                          <button
-                              className={style.btn}
-                              onClick={() => {
-                                  this.props.follow(u.id);
-                              }}
-                          >
-                              Follow
-                          </button>
-                      )}
-                    </div>
-                  </span>
-                            <span>
-                    <span>
-                      <div>{u.name}</div>
-                      <div>{u.status}</div>
-                    </span>
-                    <span>
-                      <div>{"u.location.country"}</div>
-                      <div>{"u.location.city"}</div>
-                    </span>
-                  </span>
-                        </div>
-                    ))}
+                <div>
+                  {u.followed ? (
+                      <button
+                          className={style.btn}
+                          onClick={() => {
+                              props.unfollow(u.id);
+                          }}
+                      >
+                          Unfollow
+                      </button>
+                  ) : (
+                      <button
+                          className={style.btn}
+                          onClick={() => {
+                              props.follow(u.id);
+                          }}
+                      >
+                          Follow
+                      </button>
+                  )}
                 </div>
+              </span>
+                        <span>
+                <span>
+                  <div>{u.name}</div>
+                  <div>{u.status}</div>
+                </span>
+                <span>
+                  <div>{"u.location.country"}</div>
+                  <div>{"u.location.city"}</div>
+                </span>
+              </span>
+                    </div>
+                ))}
             </div>
-        );
-    }
+            <div className={style.pages}>
+                {pages.map((p) => {
+                    return (
+                        <span
+                            className={props.currentPage === p && style.active}
+                            onClick={() => {
+                                props.onPageChanged(p);
+                            }}
+                        >
+                {p}
+              </span>
+                    );
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Users
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,29 +1,94 @@
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+import {
+  followAC,
+  setCurrentPage,
+  setIsFetchingAC,
+  setTotalUsersCountAC,
+  setUsersAC,
+  unfollowAC,
+} from "../../Redux/users-reducer";
+import axios from "axios";
 import Users from "./Users";
-import {followAC, setUsersAC, unfollowAC} from "../../Redux/users-reducer";
+import React from "react";
+import Preloader from "../common/Preloader/Preloader";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    if (this.props.users.length === 0) {
+      this.props.setIsFetching(true);
+      axios
+        .get(
+          `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+        )
+        .then((response) => {
+          this.props.setIsFetching(false);
+          this.props.setUsers(response.data.items);
+          this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    this.props.setIsFetching(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setIsFetching(false);
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render = () => {
+    return (
+      <>
+        {this.props.isFetching ? <Preloader /> : null}
+        <Users
+          currentPage={this.props.currentPage}
+          users={this.props.users}
+          unfollow={this.props.unfollow}
+          follow={this.props.follow}
+          onPageChanged={this.onPageChanged}
+        />
+      </>
+    );
+  };
+}
 
 let mapStateToProps = (state) => {
-    return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
-    }
-}
+  return {
+    users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
+    isFetching: state.usersPage.isFetching,
+  };
+};
 
 let mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followAC(userId))
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowAC(userId))
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users))
-        },
-    }
-}
+  return {
+    follow: (userId) => {
+      dispatch(followAC(userId));
+    },
+    unfollow: (userId) => {
+      dispatch(unfollowAC(userId));
+    },
+    setUsers: (users) => {
+      dispatch(setUsersAC(users));
+    },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPage(pageNumber));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setTotalUsersCountAC(totalCount));
+    },
+    setIsFetching: (isFetching) => {
+      dispatch(setIsFetchingAC(isFetching));
+    },
+  };
+};
 
 
-export default connect(mapStateToProps, mapDispatchToProps) (Users)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
